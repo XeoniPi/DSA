fd4b78117ed865942619288ba6cd8b0b47c45190
startup-Git <mdroky2016s.s.c@gmail.com>
2026-02-19 20:50:06 +0600

    Bubble sort

--- previuse_DSA_pactices.py
+++ previuse_DSA_pactices.py
@@ -29,3 +29,20 @@
 #     arr = [1,2,3,4,5,6,7,8,9]
 #     target = 8
 #     print(BinarySearch.solve(arr,target))
+
+# Bubble Sort
+class BubbleSort:
+    def solve(arr):
+        n = len(arr)
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if arr[j] > arr[j+1]:
+                    temp = arr[j]
+                    arr[j] = arr[j+1]
+                    arr[j+1] = temp
+
+if __name__ == "__main__":
+    arr = [64, 34, 25, 12, 22, 11, 90]
+    BubbleSort.solve(arr)
+    print("Sorted array is:", arr)
+    
